ce8d03604c86473d13876dc1cbfa652a8b26b124
LostandLucky <stevenrday@gmail.com>
2016-07-28 06:30:52 -0400

    renamed root static folder to staticOriginals and setup the settings file to add that directory to collectstatic search path

--- mysite/settings.py
+++ mysite/settings.py
@@ -121,7 +121,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = 'static'
+
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
+    os.path.join(BASE_DIR, "staticOriginals")
 ]
 

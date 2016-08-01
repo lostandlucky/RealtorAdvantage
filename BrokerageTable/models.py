523de53fe3ed14554e505ae758bad53e3357300f
LostandLucky <stevenrday@gmail.com>
2016-08-01 18:07:00 -0400

    added table to BrokerageTable template

--- BrokerageTable/models.py
+++ BrokerageTable/models.py
@@ -6,20 +6,20 @@ class Brokerage(models.Model):
 	#BrokerageLogo = ImageField
 	ReviewLink = models.CharField(max_length=1000)
 	ContactLink = models.CharField(max_length=1000)
-	TotalAgents = models.IntegerField
+	TotalAgents = models.IntegerField()
 	Location = models.CharField(max_length=500)
-	Desks = models.IntegerField
+	Desks = models.IntegerField()
 	YearlyCosts = models.DecimalField(max_digits=12, decimal_places=2)
 	CommisionSplit = models.CharField (max_length=8)
 	#To-Do set a getter for Cap that returns none
 	Cap =  models.DecimalField(max_digits=12, decimal_places=2)
-	TrainingPerWeek = models.IntegerField
-	Onboarding = models.BooleanField
-	Mentorship = models.BooleanField
-	Teams_Hiring = models.BooleanField
+	TrainingPerWeek = models.IntegerField()
+	Onboarding = models.BooleanField()
+	Mentorship = models.BooleanField()
+	Teams_Hiring = models.BooleanField()
 	Marketing = models.CharField(max_length=500)
-	TotalListings = models.IntegerField
-	ConferenceRooms = models.BooleanField
+	TotalListings = models.IntegerField()
+	ConferenceRooms = models.BooleanField()
 	OfficeLeaders = models.CharField (max_length=500)
 	#OfficeLeaderPhoto = models.ImageField
 
